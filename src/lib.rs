31fa635e7eb69c931489463ad402130ee263be1b
Fede García <garciafedericoagustin@gmail.com>
2024-11-27 09:18:30 +0000

    Refactor BigInt to use u8 for data representation and implement addition operation with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,57 @@
+type BigIntChunk = u8;
+
 struct BigInt {
-    data: Vec<usize>,
+    data: Vec<BigIntChunk>,
 }
 
 impl BigInt {
-    fn new(data: Vec<usize>) -> Self {
+    fn new(data: Vec<BigIntChunk>) -> Self {
         BigInt { data }
+    }
+}
+
+impl std::fmt::Debug for BigInt {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(formatter, "BigInt ({}): [", self.data.len())?;
+
+        for (index, value) in self.data.iter().enumerate() {
+            write!(formatter, "{}", value)?;
+
+            if index < self.data.len() - 1 {
+                write!(formatter, ", ")?;
+            }
+        }
+
+        write!(formatter, "]")?;
+
+        Ok(())
     }
 }
 
 impl std::cmp::PartialEq for BigInt {
     fn eq(&self, other: &Self) -> bool {
         self.data == other.data
+    }
+}
+
+impl std::ops::Add for BigInt {
+    type Output = Self;
+
+    fn add(self, right: Self) -> Self {
+        let mut result: Self = BigInt::new(vec![]);
+
+        let mut previous_carry: bool = false;
+
+        for (index, value) in self.data.iter().enumerate() {
+            let (sum1, carry1) = value.overflowing_add(previous_carry as BigIntChunk);
+            let (sum2, carry2) = sum1.overflowing_add(right.data[index]);
+
+            result.data.push(sum2);
+
+            previous_carry = carry1 || carry2;
+        }
+
+        result
     }
 }
 
@@ -55,5 +96,20 @@ mod tests {
 
         // Then
         assert_eq!(result, false);
+    }
+
+    #[test]
+    fn should_add_2_bigint() {
+        // Given
+        let a = BigInt::new(vec![0xE4, 0x08]);
+        let b = BigInt::new(vec![0xF1, 0x03]);
+
+        let expected = BigInt::new(vec![0xD5, 0x0C]);
+
+        // When
+        let result = a + b;
+
+        // Then
+        assert_eq!(result, expected);
     }
 }
