71eb4e942c68da5f9d4fc2929d5ecf2575157803
Fede García <garciafedericoagustin@gmail.com>
2024-11-27 09:17:52 +0000

    Implement PartialEq for BigInt and add equality tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,52 @@ impl BigInt {
     }
 }
 
+impl std::cmp::PartialEq for BigInt {
+    fn eq(&self, other: &Self) -> bool {
+        self.data == other.data
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn should_create_a_bigint_from_2_bytes() {
-        let bigint = BigInt::new(vec![0xE4, 0x08]);
+        // Given
+        let data = vec![0xE4, 0x08];
 
+        // When
+        let bigint = BigInt::new(data);
+
+        // Then
         assert_eq!(bigint.data[0], 0xE4);
         assert_eq!(bigint.data[1], 0x08);
+    }
+
+    #[test]
+    fn should_be_equal() {
+        // Given
+        let a = BigInt::new(vec![0xE4, 0x08]);
+        let b = BigInt::new(vec![0xE4, 0x08]);
+
+        // When
+        let result = a == b;
+
+        // Then
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn should_not_be_equal() {
+        // Given
+        let a = BigInt::new(vec![0xE4, 0x08]);
+        let b = BigInt::new(vec![0xE4]);
+
+        // When
+        let result = a == b;
+
+        // Then
+        assert_eq!(result, false);
     }
 }
